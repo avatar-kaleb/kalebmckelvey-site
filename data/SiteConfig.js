6a7d79e92a2e8a76d619922b5741cf75ea69b4dc
Kaleb McKelvey <kmckelvey3@outlook.com>
2018-06-17 13:25:53 -0500

    fix settings

--- data/SiteConfig.js
+++ data/SiteConfig.js
@@ -3,8 +3,7 @@ module.exports = {
   siteTitle: "Kaleb McKelvey", // Site title.
   siteTitleAlt: "Kaleb McKelvey", // Alternative site title for SEO.
   siteLogo: "/logos/logo-1024.png", // Logo used for SEO and manifest.
-  siteUrl: "https://kalebmckelvey.com
-  ", // Domain of your website without pathPrefix.
+  siteUrl: "https://kalebmckelvey.com", // Domain of your website without pathPrefix.
   pathPrefix: "/", // Prefixes all links. For cases when deployed to example.github.io/gatsby-material-starter/.
   fixedFooter: false, // Whether the footer component is fixed, i.e. always visible
   siteDescription: "A GatsbyJS stater with Material design in mind.", // Website description used for RSS feeds/meta description tag.
