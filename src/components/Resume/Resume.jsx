868b329abc4f4ad7dece8ba868243be650dfd82f
Kaleb McKelvey <kmckelvey3@outlook.com>
2018-06-17 22:07:45 -0500

    no message

--- src/components/Resume/Resume.jsx
+++ src/components/Resume/Resume.jsx
@@ -1,10 +1,5 @@
 import React, { Component } from "react";
 import Card from "react-md/lib/Cards/Card";
-// import { Document } from "react-pdf";
-// import CardText from "react-md/lib/Cards/CardText";
-// import UserLinks from "../UserLinks/UserLinks";
-// import config from "../../../data/SiteConfig";
-import resumePdf from "../../documents/KalebMcKelveyResume.pdf";
 import "./Resume.scss";
 
 class Resume extends Component {
@@ -13,12 +8,6 @@ class Resume extends Component {
       <div className="about-container md-grid mobile-fix">
         <Card className="md-grid md-cell--8">
           <div className="resume-wrapper" />
-          {/* <Document
-            file={{
-              url:
-                "/static/KalebMcKelveyResume-b2b8f84b61cdaaa076ddc3b5943b1164.pdf"
-            }}
-          /> */}
         </Card>
       </div>
     );
