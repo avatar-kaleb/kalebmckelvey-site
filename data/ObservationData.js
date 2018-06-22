ad32e5532fe519a4e2fb80ede93d75476ce7e068
Kaleb McKelvey <kmckelvey3@outlook.com>
2018-06-21 22:17:26 -0500

    updated resource components

--- data/ObservationData.js
+++ data/ObservationData.js
@@ -31,5 +31,35 @@ module.exports = [
     id: 4,
     title: "Actions Always Speak Like Pictures Describe",
     value: `People in your life can tell you how much they respect you, care about you, appreciate you, etc. These declarations are great and an important part of friendships, yet it is always the actions that either dispel or backup these statements that truly matter.`
+  },
+  {
+    id: 5,
+    title: "Spending Time > Spending Money",
+    value:
+      "You can always do more to increase the amount of money you have in bank accounts, but there is an account that decreases every second. Your time balance. There is nothing more important than giving the people you love in your life some of those ticking seconds."
+  },
+  {
+    id: 6,
+    title: "Measurements in Your Life",
+    value:
+      "We each use different measurements for other people in our life, friendships or spouses as we want to have balance within those relationships. One key point to remember? You can’t control others and if they don’t have the same measurements in place – you may end up disappointed often. Communicate those expectations if necessary, or accept them for who they are. The boundary should be of their character, not the measurements of their contact."
+  },
+  {
+    id: 7,
+    title: "Social Media Doesn’t Paint the Truth",
+    value:
+      "Taking a peek at someone’s social media accounts as an indicator of their life doesn’t work. People post in different ways, only show specific information, and there is no true connection in a page visit. Real friendships are those who catch up through face to face or phone calls that produce intimate conversations to build trust. There is no other way."
+  },
+  {
+    id: 8,
+    title: "Imposter Syndrome is Real",
+    value:
+      "Expertise is a fluid concept that doesn’t have defined expectations. In reality, most of the time, there is someone who is more expert than you on a subject. Let them be and live your life in peace. Instead, do your best to get a little smart, and a little wiser each day. As you get closer and closer to your life expectancy…you will be smarter for it."
+  },
+  {
+    id: 9,
+    title: "A Key to a Good Life is Avoiding Big Mistakes",
+    value:
+      "Mistakes are going to happen in your life, there is no mistaking that. Decide where you want to be at the end of your life and start using inversion to see which actions take you there. For each action, ask yourself – what would be detrimintal to reaching these goals….avoid them like the plague, or bitcoin."
   }
 ];
