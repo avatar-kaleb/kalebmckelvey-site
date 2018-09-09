6e9fcf078801ed506bc00a1ca8016be1b27daee5
Kaleb McKelvey <kmckelvey3@outlook.com>
2018-09-09 10:29:57 -0500

    updated observations and inspiration

--- data/ObservationData.js
+++ data/ObservationData.js
@@ -13,7 +13,7 @@ module.exports = [
     title: "Doing Something Lessons",
     value: `Sometimes just doing something before you
     actually have the full plan ready results in better focus to grow
-    the plan, or the fail fast mentality of projects you don't
+    the plan, or fail fast on a project you don't
     actually enjoy - meaning you don't spend a bunch of time planning
     for something you find out you dislike.`
   },
@@ -25,12 +25,12 @@ module.exports = [
     compound interest, gradually growing into the high levels you
     eventually see. This gives more reason to not procrastinate, but
     break down big tasks into smaller daily ones (like do 1 hour of
-    training a day vs 4 on Friday).`
+    training a day vs 4 on Friday) to achieve our goals.`
   },
   {
     id: 4,
     title: "Actions Always Speak Like Pictures Describe",
-    value: `People in your life can tell you how much they respect you, care about you, appreciate you, etc. These declarations are great and an important part of friendships, yet it is always the actions that either dispel or backup these statements that truly matter.`
+    value: `People in your life can tell you how much they respect you, care about you, appreciate you, etc. These declarations are great and an important part of relationships, yet it is always the actions that either dispel or backup these statements that truly matter.`
   },
   {
     id: 5,
@@ -42,19 +42,19 @@ module.exports = [
     id: 6,
     title: "Measurements in Your Life",
     value:
-      "We each use different measurements for other people in our life, friendships or spouses as we want to have balance within those relationships. One key point to remember? You can’t control others and if they don’t have the same measurements in place – you may end up disappointed often. Communicate those expectations if necessary, or accept them for who they are. The boundary should be of their character, not the measurements of their contact."
+      "We each use different measurements for other people in our life, friendships or spouses as we want to have balance within those relationships. One key point to remember? You can’t control others and if they don’t have the same measurements in place – you may end up disappointed. Communicate those expectations if necessary, or accept them for who they are. The boundary should be of their character, not the measurements of their contact or other expectations you set on them."
   },
   {
     id: 7,
     title: "Social Media Doesn’t Paint the Truth",
     value:
-      "Taking a peek at someone’s social media accounts as an indicator of their life doesn’t work. People post in different ways, only show specific information, and there is no true connection in a page visit. Real friendships are those who catch up through face to face or phone calls that produce intimate conversations to build trust. There is no other way."
+      "Taking a peek at someone’s social media accounts as an indicator of their life doesn’t work. People post in different ways, only show specific information, and there isn't a true connection in a page visit. Real friendships are those who catch up through face to face or phone calls that produce intimate conversations to build trust. There is no other way."
   },
   {
     id: 8,
     title: "Imposter Syndrome is Real",
     value:
-      "Expertise is a fluid concept that doesn’t have defined expectations. In reality, most of the time, there is someone who is more expert than you on a subject. Let them be and live your life in peace. Instead, do your best to get a little smart, and a little wiser each day. As you get closer and closer to your life expectancy…you will be smarter for it."
+      "Expertise is a fluid concept that doesn’t have defined expectations. In reality, most of the time, there is someone who has more expertise than you on a subject. Let them be and live your life in peace. Instead, do your best to get a little smarter, and a little wiser each day."
   },
   {
     id: 9,
