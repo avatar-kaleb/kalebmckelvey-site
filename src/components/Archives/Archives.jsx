c51d3694f0fa861b33c58ad353a34cdcab209b11
Kaleb McKelvey <kmckelvey3@outlook.com>
2018-12-24 13:28:46 -0500

    adding in search to archives page

--- src/components/Archives/Archives.jsx
+++ src/components/Archives/Archives.jsx
@@ -7,7 +7,7 @@ import PostListing from '../PostListing/PostListing';
  */
 const Archives = ({ postEdges }) => (
   <div className="archives animated fadeIn slow">
-    <PostListing postEdges={postEdges} />
+    <PostListing postEdges={postEdges} withSearch />
   </div>
 );
 
