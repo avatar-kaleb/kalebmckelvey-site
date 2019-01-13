066650aba88748ce98ea9acabd46291b16db150c
Kaleb McKelvey <kmckelvey3@outlook.com>
2019-01-13 15:31:16 -0500

    fixing margin on mobile for post listing

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -35,7 +35,7 @@ class Home extends Component {
 
     return (
       <section className="home mobile-fix animated fadeIn slow">
-        <Paper className="md-grid paper-background--white" zDepth={0}>
+        <Paper className="md-grid paper-background--white intro-card" zDepth={0}>
           <Cell align="middle" desktopOffset={1} desktopSize={3} tabletSize={3} phoneSize={4} position="center">
             <AvatarImage src={kalebWelcomeImage} alt="Kaleb with a blue shirt on with arms wide open smiling!" />
           </Cell>
