4baaceed15cae7820e8733bdeca89e0ac57b2c7d
Kaleb McKelvey <kmckelvey3@outlook.com>
2019-02-03 14:31:16 -0500

    adding new observation

--- data/ObservationData.js
+++ data/ObservationData.js
@@ -67,5 +67,11 @@ export default [
     title: 'Experiences > Materials - Spend Wisely',
     value:
       'During the most memorable experiences in your life, no one remembers what you were wearing, what size watch you were wearing, or what brand of shoes you have on. They remember how they felt, who they were with, and the new experience they never had before!'
+  },
+  {
+    id: 11,
+    title: 'Journey Before Destination',
+    value:
+      '“The most important words a man can say are, “I will do better.” These are not the most important words any man can say. I am a man, and they are what I needed to say. The ancient code of the Knights Radiant says “journey before destination.” Some may call it a simple platitude, but it is far more.A journey will have pain and failure.It is not only the steps forward that we must accept.It is the stumbles.The trials.The knowledge that we will fail.That we will hurt those around us. But if we stop, if we accept the person we are when we fall, the journey ends.That failure becomes our destination.To love the journey is to accept no such end.I have found, through painful experience, that the most important step a person can take is always the next one." -Brandon Sanderson Oathbringer'
   }
 ];
