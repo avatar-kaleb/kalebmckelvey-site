4395e2a79704bd716c3cbc0e11fa38ec3123bc08
Kaleb McKelvey <kmckelvey3@outlook.com>
2019-10-29 20:18:15 -0400

    “adding the filter for non published blogs to categories”

--- src/templates/category.jsx
+++ src/templates/category.jsx
@@ -30,7 +30,7 @@ export const pageQuery = graphql`
     allMarkdownRemark(
       limit: 1000
       sort: { fields: [frontmatter___date], order: DESC }
-      filter: { frontmatter: { category: { eq: $category } } }
+      filter: { frontmatter: { category: { eq: $category }, published: { eq: true } } }
     ) {
       totalCount
       edges {
