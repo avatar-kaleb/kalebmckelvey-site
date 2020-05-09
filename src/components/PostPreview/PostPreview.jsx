06143f161612eab93a508eabd725d7f1832a1f22
Kaleb McKelvey <kmckelvey3@outlook.com>
2020-05-09 15:43:29 -0400

    “changing more class components to functional”

--- src/components/PostPreview/PostPreview.jsx
+++ src/components/PostPreview/PostPreview.jsx
@@ -1,5 +1,5 @@
 import { navigate } from 'gatsby';
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import Avatar from 'react-md/lib/Avatars';
 import Button from 'react-md/lib/Buttons';
 import Card from 'react-md/lib/Cards/Card';
@@ -22,110 +22,82 @@ import './PostPreview.scss';
  */
 const navToPostOnClick = (path, e) => navigate(path);
 
-/**
- * Post Preview is used to show summary of posts on a post listening to give a quick glimpse
- * of what the post is about
- */
-class PostPreview extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      buttonHidden: false,
-      isMobile: true
-    };
+const PostPreview = ({ postInfo }) => {
+  const [buttonHidden, setButtonHidden] = useState(false);
+  const [isMobile, setIsMobile] = useState(true);
+  const coverHeights = {
+    mobile: 162,
+    other: 225
+  };
+  /* eslint no-undef: "off" */
+  const cover =
+    postInfo.cover.substring(0, 1) === '/' ? __PATH_PREFIX__ + postInfo.cover : postInfo.cover;
+  const coverHeight = isMobile ? coverHeights.mobile : coverHeights.other;
 
-    this.coverHeights = {
-      mobile: 162,
-      other: 225
-    };
-
-    this.setButtonHidden = this.setButtonHidden.bind(this);
-    this.handleResize = this.handleResize.bind(this);
-  }
-
-  componentDidMount() {
-    this.handleResize();
-  }
-
-  /**
-   * Ths sets the button for this post preview to show or hide
-   * We need this to use with particle effect button
-   */
-  setButtonHidden() {
-    this.setState((prevState) => ({ buttonHidden: !prevState.buttonHidden }));
-  }
+  // run once on mount
+  useEffect(() => {
+    handleResize();
+  }, []);
 
   /**
    * Set mobile state based on size changes
    */
-  handleResize() {
+  const handleResize = () => {
     if (window.innerWidth >= screenWidths.maxMobile) {
-      if (this.state.isMobile) {
-        this.setState({ isMobile: false });
+      if (isMobile) {
+        setIsMobile(false);
       }
     } else {
-      if (!this.state.isMobile) {
-        this.setState({ isMobile: true });
+      if (!isMobile) {
+        setIsMobile(true);
       }
     }
-  }
+  };
 
-  render() {
-    const { buttonHidden, isMobile } = this.state;
-    const { postInfo } = this.props;
-    /* eslint no-undef: "off" */
-    const cover =
-      postInfo.cover.substring(0, 1) === '/' ? __PATH_PREFIX__ + postInfo.cover : postInfo.cover;
-    const coverHeight = isMobile ? this.coverHeights.mobile : this.coverHeights.other;
+  return (
+    <div className="md-cell md-cell--0-desktop-offset md-cell--0-phone-offset md-cell--4 md-cell--center post-preview margin-bottom--small">
+      <Card key={postInfo.path} animate={false} className="">
+        <Media
+          style={{
+            backgroundImage: `url(${cover})`,
+            height: `${coverHeight}px`
+          }}
+          className="post-preview-cover"
+          onClick={(e) => navToPostOnClick(postInfo.path, e)}
+        >
+          <MediaOverlay>
+            <CardTitle title={postInfo.title} onClick={(e) => navToPostOnClick(postInfo.path, e)} />
+          </MediaOverlay>
+        </Media>
+        <CardTitle
+          expander={isMobile}
+          avatar={<Avatar icon={<FontIcon iconClassName="fa fa-calendar" />} />}
+          title={`Published on ${postInfo.date}`}
+          subtitle={`${postInfo.timeToRead} min read`}
+        />
+        <CardText expandable={isMobile} className="card-text">
+          <p>{postInfo.excerpt}</p>
+          <PostTags tags={postInfo.tags} />
+        </CardText>
 
-    return (
-      <div className="md-cell md-cell--0-desktop-offset md-cell--0-phone-offset md-cell--4 md-cell--center post-preview margin-bottom--small">
-        <Card key={postInfo.path} animate={false} className="">
-          <Media
-            style={{
-              backgroundImage: `url(${cover})`,
-              height: `${coverHeight}px`
-            }}
-            className="post-preview-cover"
-            onClick={(e) => navToPostOnClick(postInfo.path, e)}
+        <CardActions className="md-divider-border md-divider-border--top">
+          <ParticleEffectButton
+            color={particleConfig.color}
+            duration={particleConfig.duration}
+            hidden={buttonHidden}
+            onComplete={(e) => navToPostOnClick(postInfo.path, e)}
+            oscillationCoefficient={particleConfig.oscillationCoefficient}
+            particlesAmountCoefficient={particleConfig.particlesAmountCoefficient}
           >
-            <MediaOverlay>
-              <CardTitle
-                title={postInfo.title}
-                onClick={(e) => navToPostOnClick(postInfo.path, e)}
-              />
-            </MediaOverlay>
-          </Media>
-          <CardTitle
-            expander={isMobile}
-            avatar={<Avatar icon={<FontIcon iconClassName="fa fa-calendar" />} />}
-            title={`Published on ${postInfo.date}`}
-            subtitle={`${postInfo.timeToRead} min read`}
-          />
-          <CardText expandable={isMobile} className="card-text">
-            <p>{postInfo.excerpt}</p>
-            <PostTags tags={postInfo.tags} />
-          </CardText>
-
-          <CardActions className="md-divider-border md-divider-border--top">
-            <ParticleEffectButton
-              color={particleConfig.color}
-              duration={particleConfig.duration}
-              hidden={buttonHidden}
-              onComplete={(e) => navToPostOnClick(postInfo.path, e)}
-              oscillationCoefficient={particleConfig.oscillationCoefficient}
-              particlesAmountCoefficient={particleConfig.particlesAmountCoefficient}
-            >
-              {/* Raised looked better, outside of MD spec though */}
-              <Button raised primary onClick={this.setButtonHidden}>
-                Read more!
-              </Button>
-            </ParticleEffectButton>
-          </CardActions>
-        </Card>
-      </div>
-    );
-  }
-}
+            {/* Raised looked better, outside of MD spec though */}
+            <Button raised primary onClick={setButtonHidden(!buttonHidden)}>
+              Read more!
+            </Button>
+          </ParticleEffectButton>
+        </CardActions>
+      </Card>
+    </div>
+  );
+};
 
 export default PostPreview;
--- src/components/SignupForm/SignupForm.jsx
+++ src/components/SignupForm/SignupForm.jsx
@@ -1,169 +1,143 @@
-import React, { Component } from 'react';
 import addToMailchimp from 'gatsby-plugin-mailchimp';
+import debounce from 'lodash/debounce';
+import React, { useState } from 'react';
+import { Snackbar } from 'react-md';
 import Button from 'react-md/lib/Buttons/Button';
 import Cell from 'react-md/lib/Grids/Cell';
-import debounce from 'lodash/debounce';
 import Grid from 'react-md/lib/Grids/Grid';
-import ParticleEffectButton from 'react-particle-effect-button';
 import TextField from 'react-md/lib/TextFields/TextField';
-import { Snackbar } from 'react-md';
-
+import ParticleEffectButton from 'react-particle-effect-button';
 import { particleConfig } from '../../constants/config';
 import './SignupForm.scss';
 
 /**
  * Sign up form to subscribe users to mail chimp list
  */
-class SignupForm extends Component {
-  _handleFirstNameChange = debounce(e => {
-    this.setState({ firstName: e });
+const SignupForm = () => {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [status, setStatus] = useState('');
+  const [hidden, setHidden] = useState(false);
+  const [toasts, setToasts] = useState([]);
+
+  const _handleFirstNameChange = debounce((e) => {
+    setFirstName(e);
   }, 500);
 
-  _handleLastNameChange = debounce(e => {
-    this.setState({ lastName: e });
+  const _handleLastNameChange = debounce((e) => {
+    setLastName(e);
   }, 500);
 
-  _handleEmailChange = debounce(e => {
-    this.setState({ email: e });
+  const _handleEmailChange = debounce((e) => {
+    setEmail(e);
   }, 500);
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      firstName: ``,
-      lastName: '',
-      email: '',
-      status: '',
-      hidden: false,
-      toasts: []
-    };
-  }
-
-  _dismissToasts = () => {
-    this.setState({ toasts: [] });
+  const _dismissToasts = () => {
+    setToasts([]);
   };
 
   // taken from Gatsby Mail Form repo
-  _postEmailToMailchimp = (email, attributes) => {
+  const _postEmailToMailchimp = (email, attributes) => {
     addToMailchimp(email, attributes)
-      .then(result => {
+      .then((result) => {
         // Mailchimp always returns a 200 response
         // So we check the result for MC errors & failures
         if (result.result !== `success`) {
-          this.setState({
-            status: `error`,
-            hidden: false,
-            toasts: [{ text: 'There was an error subscribing - please retry!' }]
-          });
+          setStatus('error');
+          setHidden(false);
+          setToasts([{ text: 'There was an error subscribing - please retry!' }]);
         } else {
           // Email address succesfully subcribed to Mailchimp
-          this.setState({
-            status: `success`
-          });
+          setStatus('success');
         }
       })
       .catch(() => {
         // Network failures, timeouts, etc
-        this.setState({
-          status: `error`,
-          hidden: false,
-          toasts: [{ text: 'There was an error subscribing - please retry!' }]
-        });
+        setStatus('error');
+        setHidden(false);
+        setToasts([{ text: 'There was an error subscribing - please retry!' }]);
       });
   };
 
-  _handleFormSubmit = e => {
+  const _handleFormSubmit = (e) => {
     e.preventDefault();
     e.stopPropagation();
 
-    const { email, firstName, lastName } = this.state;
-
     if (email && firstName) {
-      this.setState(
-        {
-          status: `sending`,
-          hidden: true
-        },
-        // setState callback (subscribe email to MC)
-        this._postEmailToMailchimp(email, {
-          FNAME: firstName,
-          LNAME: lastName
-        })
-      );
+      setStatus('sending');
+      setHidden(true);
+      _postEmailToMailchimp(email, {
+        FNAME: firstName,
+        LNAME: lastName
+      });
     }
   };
 
-  render() {
-    const { hidden, status, toasts } = this.state;
-    return (
-      <div className="signup-form">
-        <Snackbar id="example-snackbar" toasts={toasts} autohide onDismiss={this._dismissToasts} />
-        {status === 'success' ? (
-          <p className="animated lightSpeedIn">
-            Success! Thank you for joining - may the
-            {' '}
-            <em>achievement</em>
-            {' '}
-force be with you!
-          </p>
-        ) : (
-          <div className="animated fadeInRight">
-            <p className="form-p--mb">Want to stay updated on new posts? Subscribe below!</p>
-            <br />
-            <form id="email-capture" method="post">
-              <Grid className="form--border">
-                <Cell size={4}>
-                  <TextField
-                    id="floating-center-title"
-                    errorText="This field is required."
-                    label="First Name"
-                    lineDirection="center"
-                    placeholder="George"
-                    required
-                    onChange={this._handleFirstNameChange}
-                  />
-                </Cell>
-                <Cell size={4}>
-                  <TextField
-                    id="floating-center-title"
-                    label="Last Name"
-                    lineDirection="center"
-                    placeholder="Castanza"
-                    onChange={this._handleLastNameChange}
-                  />
-                </Cell>
-                <Cell size={4}>
-                  <TextField
-                    id="floating-center-title"
-                    errorText="This field is required."
-                    label="Email Address"
-                    lineDirection="center"
-                    placeholder="George@test.com"
-                    onChange={this._handleEmailChange}
-                    required
-                  />
-                </Cell>
-                <Cell size={12} position="right">
-                  <ParticleEffectButton
-                    className="md-cell--right"
-                    color={particleConfig.color}
-                    duration={particleConfig.duration}
-                    hidden={hidden}
-                    oscillationCoefficient={particleConfig.oscillationCoefficient}
-                    particlesAmountCoefficient={particleConfig.particlesAmountCoefficient}
-                  >
-                    <Button className="form-button--mt" raised primary onClick={this._handleFormSubmit}>
-                      Subscribe!
-                    </Button>
-                  </ParticleEffectButton>
-                </Cell>
-              </Grid>
-            </form>
-          </div>
-        )}
-      </div>
-    );
-  }
-}
+  return (
+    <div className="signup-form">
+      <Snackbar id="example-snackbar" toasts={toasts} autohide onDismiss={_dismissToasts} />
+      {status === 'success' ? (
+        <p className="animated lightSpeedIn">
+          Success! Thank you for joining - may the <em>achievement</em> force be with you!
+        </p>
+      ) : (
+        <div className="animated fadeInRight">
+          <p className="form-p--mb">Want to stay updated on new posts? Subscribe below!</p>
+          <br />
+          <form id="email-capture" method="post">
+            <Grid className="form--border">
+              <Cell size={4}>
+                <TextField
+                  id="floating-center-title"
+                  errorText="This field is required."
+                  label="First Name"
+                  lineDirection="center"
+                  placeholder="George"
+                  required
+                  onChange={_handleFirstNameChange}
+                />
+              </Cell>
+              <Cell size={4}>
+                <TextField
+                  id="floating-center-title"
+                  label="Last Name"
+                  lineDirection="center"
+                  placeholder="Castanza"
+                  onChange={_handleLastNameChange}
+                />
+              </Cell>
+              <Cell size={4}>
+                <TextField
+                  id="floating-center-title"
+                  errorText="This field is required."
+                  label="Email Address"
+                  lineDirection="center"
+                  placeholder="George@test.com"
+                  onChange={_handleEmailChange}
+                  required
+                />
+              </Cell>
+              <Cell size={12} position="right">
+                <ParticleEffectButton
+                  className="md-cell--right"
+                  color={particleConfig.color}
+                  duration={particleConfig.duration}
+                  hidden={hidden}
+                  oscillationCoefficient={particleConfig.oscillationCoefficient}
+                  particlesAmountCoefficient={particleConfig.particlesAmountCoefficient}
+                >
+                  <Button className="form-button--mt" raised primary onClick={_handleFormSubmit}>
+                    Subscribe!
+                  </Button>
+                </ParticleEffectButton>
+              </Cell>
+            </Grid>
+          </form>
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default SignupForm;
