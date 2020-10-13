ad63bd6354972e5f2f827e9ef2001c192af6b522
Kaleb McKelvey <kmckelvey3@outlook.com>
2020-10-12 21:07:51 -0400

    “fixing image bug”

--- src/components/PostCover/PostCover.jsx
+++ src/components/PostCover/PostCover.jsx
@@ -7,15 +7,17 @@ import './PostCover.scss';
  * @param {Boolean} isMobile - is this a mobile cover
  */
 const PostCover = ({ postNode, isMobile }) => {
+  debugger;
   const post = postNode.frontmatter;
   /* eslint no-undef: "off" */
-  const cover = post.cover.substring(0, 1) === '/' ? __PATH_PREFIX__ + post.cover : post.cover;
+  const cover =
+    post.cover.substring(0, 1) === '/' ? __PATH_PREFIX__ + post.cover : '/' + post.cover;
   const coverHeight = isMobile ? 180 : 350;
 
   return (
     <div
       style={{ backgroundImage: `url(${cover})`, height: `${coverHeight}px` }}
-      className="md-grid md-cell--10 post-cover"
+      className='md-grid md-cell--10 post-cover'
     />
   );
 };
